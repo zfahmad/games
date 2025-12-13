ce9728adf67bf8f476f87b615e7843c36ee99e89
Zaheen <zaheen.fa@gmail.com>
2025-12-13 09:56:11 -0700

    Started TicTacToe.

---
+++ build/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o.d"
+  "tic_tac_toe"
+  "tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/games.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zaheen/projects/games/src/main.cpp" "src/CMakeFiles/games.dir/main.cpp.o" "gcc" "src/CMakeFiles/games.dir/main.cpp.o.d"
+  "/Users/zaheen/projects/games/src/tic_tac_toe.cpp" "src/CMakeFiles/games.dir/tic_tac_toe.cpp.o" "gcc" "src/CMakeFiles/games.dir/tic_tac_toe.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
